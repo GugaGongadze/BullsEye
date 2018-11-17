0b278581e4964ccbb71974ff8c24dd4977751fea
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-17 18:38:37 +0400

    Feat: alert

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func alertHello() {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 
 }
