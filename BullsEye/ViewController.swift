0a7ad03b7bc53eeee834ee43a07df55227e3159b
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-17 22:01:31 +0400

    Feat: keep track of total score

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     @IBAction func alertHello() {
         let difference = abs(targetValue - currentValue)
         let points = 100 - difference
+        score += points
         
         let message = "You scored \(points) points"
         
