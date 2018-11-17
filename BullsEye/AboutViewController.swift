6a5bfd6a9ce12b501cdad91998a175f1c7a24446
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-17 23:10:02 +0400

    Feat: game and author info pages

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Guga Gongadze on 11/17/18.
+//  Copyright © 2018 Guga Gongadze. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ BullsEye/AuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Guga Gongadze on 11/17/18.
+//  Copyright © 2018 Guga Gongadze. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
