a4ceb02330768eb68f03f6c565b279f68de238a2
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-17 19:29:35 +0400

    Feat: show slider value on alert

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +18,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func alertHello() {
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app", preferredStyle: .alert)
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The value of the slider is: \(slider.value)")
+        let rounderValue = slider.value.rounded()
+        currentValue = Int(rounderValue)
     }
 }
 
