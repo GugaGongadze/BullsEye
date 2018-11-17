78ef98b1aeaa6b78ee72df444c502c8821706bb7
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-11-17 19:42:28 +0400

    Feat: set random number to target value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue: Int = 0
     
     @IBOutlet weak var slider: UISlider!
 
@@ -18,19 +19,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let rounderValue = slider.value.rounded()
         currentValue = Int(rounderValue)
+        startNewRound()
     }
     
     @IBAction func alertHello() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
+        
         let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         let rounderValue = slider.value.rounded()
         currentValue = Int(rounderValue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 }
 
